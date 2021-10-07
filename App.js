8c3f1060ed418b1401f970584c4cdf05546188e3
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-07 08:56:25 +0300

    Minor fix (true -> false)

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/homeScreen';
-import ProfileScreen from './screens/profileScreen';
-import RegisterScreen from './screens/Auth/registerScreen';
-import SigninScreen from './screens/Auth/signinScreen';
-import ChatScreen from './screens/chatScreen';
-import WelcomeScreen from './screens/welcomeScreen';
+import React, { useState } from "react";
+import { Button, View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/homeScreen";
+import ProfileScreen from "./screens/profileScreen";
+import RegisterScreen from "./screens/Auth/registerScreen";
+import SigninScreen from "./screens/Auth/signinScreen";
+import ChatScreen from "./screens/chatScreen";
+import WelcomeScreen from "./screens/welcomeScreen";
 
 // https://reactnavigation.org/docs/auth-flow
 // Authentication flow
@@ -15,33 +15,32 @@ import WelcomeScreen from './screens/welcomeScreen';
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const [isSignedIn, setisSignedIn] = useState(true);
-  return (
-    isSignedIn ? (
-      // No token found, user isn't signed in
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-          <Stack.Screen name="Chat" component={ChatScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    ) : (
-      // User is signed in
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Kinder" component={WelcomeScreen} />
-          <Stack.Screen name="Signin" component={SigninScreen} />
-          <Stack.Screen name="Register" component={RegisterScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    ));
+  const [isSignedIn, setisSignedIn] = useState(false);
+  return isSignedIn ? (
+    // No token found, user isn't signed in
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  ) : (
+    // User is signed in
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Kinder" component={WelcomeScreen} />
+        <Stack.Screen name="Signin" component={SigninScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 24
-  }
+    marginTop: 24,
+  },
 });
